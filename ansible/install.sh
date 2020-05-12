9d4cfb62ae9c0b1894939087a393f273a23fbf65
Vlad Krokhin <3v1lh3x@gmail.com>
2020-05-12 13:35:14 +0300

    add: docker installation

--- ansible/install.sh
+++ ansible/install.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ansible-playbook --connection=local --inventory 127.0.0.1, site.yml --ask-become-pass
+ansible-playbook --connection=local --inventory 127.0.0.1, site.yml --ask-become-pass --verbose
